3c8206b0eee224134dea756ea7660ebc4892ea12
ldpeixoto <ldpeixoto@uea.edu.br>
2026-09-06 01:16:54 -0400

    chore: configure Cypress 16 (retries, default browser, expose, Allure plugin)

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,26 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  defaultCommandTimeout: 10000,
+  defaultBrowser: 'chrome',
+  expose: {
+    apiUrl: 'https://serverest.dev',
+  },
   e2e: {
+    baseUrl: 'https://front.serverest.dev/login',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    env: {
+      allure: true,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
